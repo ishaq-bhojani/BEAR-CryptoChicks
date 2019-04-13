9a105c0b48504e4f8e5ee88b1f83bb84d8d9a509
IshaqBhojani <ishaqbhojani@gmail.com>
2019-04-13 14:20:25 +0500

    propery list UI and added fields

--- src/app/components/property-listing/property-listing.component.ts
+++ src/app/components/property-listing/property-listing.component.ts
@@ -11,16 +11,22 @@ export class PropertyListingComponent implements OnInit {
       type: 'Shop',
       area: '200 Sq.Feet',
       address: 'Shop # 23, Shahrah-e-Faisal, Karachi',
+      status: false, // false for closed bidding and true for open bidding
+      changeDate: '5 May 2020',
       highestBid: 'PKR 17,000/-'
     }, {
       type: 'Home',
       area: '120 Sq.Yards',
       address: 'H # 51, Block B, Liaquat Abad, Karachi',
+      status: true, // false for closed bidding and true for open bidding
+      changeDate: '20 April 2019',
       highestBid: 'PKR 50,000/-'
     }, {
       type: 'Shop',
       area: '600 Sq.Feet',
       address: 'Shop # 23, First floor, Abdullah Mobile Mall, Clifton, Karachi',
+      status: true, // false for closed bidding and true for open bidding
+      changeDate: '26 April 2019',
       highestBid: 'PKR 30,000/-'
     }
   ];
