810f8803a030ffcc9851a53c0dfe8035cfae424f
IshaqBhojani <ishaqbhojani@gmail.com>
2019-04-13 13:46:01 +0500

    get data from angular instead of html

--- src/app/components/property-listing/property-listing.component.ts
+++ src/app/components/property-listing/property-listing.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-property-listing',
@@ -6,8 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-listing.component.css']
 })
 export class PropertyListingComponent implements OnInit {
+  properties = [
+    {
+      type: 'Shop',
+      area: '200 Sq.Feet',
+      address: 'Shop # 23, Shahrah-e-Faisal, Karachi',
+      highestBid: 'PKR 17,000/-'
+    }, {
+      type: 'Home',
+      area: '120 Sq.Yards',
+      address: 'H # 51, Block B, Liaquat Abad, Karachi',
+      highestBid: 'PKR 50,000/-'
+    }, {
+      type: 'Shop',
+      area: '600 Sq.Feet',
+      address: 'Shop # 23, First floor, Abdullah Mobile Mall, Clifton, Karachi',
+      highestBid: 'PKR 30,000/-'
+    }
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
