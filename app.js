32f9754bbc127743ec4467979903340876f4f006
IshaqBhojani <ishaqbhojani@gmail.com>
2019-04-13 15:08:03 +0500

    prevent sleep

--- app.js
+++ app.js
@@ -34,6 +34,10 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+const http = require("http");
+setInterval(function() {
+  http.get("http://bear-crypto.herokuapp.com");
+}, 300000);
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
